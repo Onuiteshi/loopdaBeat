24ef16a9e5266ea49fd30682d77b88dfac840216
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-06-25 22:54:43 +0100

    Create Splash screen

---
+++ screens/SplashScreen/style.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+
+
+export const styles = StyleSheet.create({
+    splashContainer : {
+        flex:1,
+        
+    },
+    backgroundStyle:{
+        flex:1,
+        justifyContent: 'space-around',
+        alignItems:'center',
+
+    },
+    gradient:{
+        flex:1,
+        justifyContent: 'space-around',
+        alignItems:'center',
+        width:'100%'
+
+    },
+    imageStyle:{
+        
+    },
+    buttonStyle:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        height:58,
+        width:220,
+        borderRadius:35,
+        backgroundColor:'#671B58',
+        borderWidth:1,
+        borderColor:'white',
+        borderStyle:'solid'
+        
+    },
+    textStyle:{
+        fontSize:20,
+        color:'white',
+
+    }
+
+
+
+})
