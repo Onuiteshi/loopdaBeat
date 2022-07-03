915eede4aa3cc4d266c954a025c597b233e68976
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-07-03 22:29:51 +0100

    Create loop list screen

---
+++ screens/LoopList/index.js
@@ -0,0 +1,30 @@
+import {View, useWindowDimensions} from 'react-native'
+
+import Background from '../../components/BackGround'
+import ScreenHeader from '../../components/ScreenHeader'
+import SongItems from '../../components/SongItems'
+import { assets } from '../../assets'
+
+
+function LoopList(){
+    const { height } = useWindowDimensions();
+    return(
+
+        <Background
+        style={{ justifyContent: "flex-start"}}
+        image={assets.splashBackground}
+        gradients={["rgba(103,27,88,.8)", "transparent"]}
+        >
+     <ScreenHeader
+        imageStyle={{ width: 138, height: 112 }}
+        image={assets.logoSmall}
+        headerText="LOOP LIST"
+        textStyle={{color:'white', fontSize:25, backgroundColor:'red', padding:10, margin:'auto'}}
+        height={height*0.22}
+        />
+      <SongItems/>
+
+    </Background>
+    )
+}
+export default LoopList
